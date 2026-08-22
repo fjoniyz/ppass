c184e342583e715f18d6a4f8ed6658218ab39ddc
fjoniyz <fjoniyzeiri@gmail.com>
2026-08-22 15:50:55 +0200

    Chroot of the container to rootfs + install Python to rootfs

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"os"
 
 	"private_paas/cmd"
+	"private_paas/parser"
 )
 
 func main() {
+	if os.Getenv("_PAAS_CONTAINER_INIT") == "1" {
+		parser.RunContainerInit(os.Getenv("_PAAS_SERVICE_PATH"))
+		return
+	}
+
 	if err := cmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
